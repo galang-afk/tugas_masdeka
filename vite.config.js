31e1b845677c7025d363553e78334abf8a915567
deka galang <30586485+dekagalang@users.noreply.github.com>
2025-12-28 07:14:14 +0000

    fix error build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/index.jsx'],
+            input: ['resources/js/index.jsx'],
             refresh: true,
         }),
         tailwindcss(),
