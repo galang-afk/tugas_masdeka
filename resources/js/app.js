47c54ea3b7bee15993babd6a4242abd8315cd1a1
Galang <galang34adi@gmail.com>
2025-12-28 12:19:49 +0700

    bug Database

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+function App() {
+    return <h1>React jalan di Laravel 🚀</h1>
+}
+
+createRoot(document.getElementById('app')).render(<App />)
